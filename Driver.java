b155b5ed26b1941055a295511bc27c523aa35762
mosesCircumstein <h.almond@lancaster.ac.uk>
2017-06-18 15:50:05 +0100

    created jframe

--- Driver.java
+++ Driver.java
@@ -2,6 +2,7 @@ public class Driver
 {
     public static void main(String[] Args)
     {
+        Grid g = new Grid();
         System.out.println("Pokemon");
     }
 }
---
+++ Grid.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class Grid extends JFrame
+{
+    public Grid()
+    {
+        setTitle("Pokemon Tic Tac Toe");
+        setSize(400,400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
