70745300bc485b3946400b4106305c5dca76d40e
mosesCircumstein <h.almond@lancaster.ac.uk>
2017-06-18 20:00:23 +0100

    Added actionlisteners and icons to buttons

--- Grid.java
+++ Grid.java
@@ -1,25 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Grid extends JFrame
-{
+public class Grid extends JFrame implements ActionListener {
     private JPanel panel;
     private GridLayout gLayout;
     private JButton[][] buttonArray = new JButton[3][3];
-    public Grid()
-    {
+    private Icon icon;
+
+    public Grid() {
         //creating panel and layout
-        gLayout = new GridLayout(3,3);
+        gLayout = new GridLayout(3, 3);
         panel = new JPanel();
         panel.setLayout(gLayout);
+        icon = new Icon();
         //initialising buttons
-        for(int i = 0; i<3; i++)
-        {
-            for(int j = 0; j<3; j++)
-            {
-                    buttonArray[i][j] = new JButton();
-                    buttonArray[i][j].setText(i + " " + j);
-                    panel.add(buttonArray[i][j]);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttonArray[i][j] = new JButton();
+                buttonArray[i][j].setText(i + " " + j);
+                panel.add(buttonArray[i][j]);
+                buttonArray[i][j].setIcon(icon.zeroIcon);
+                buttonArray[i][j].addActionListener(this);
             }
         }
 
@@ -27,8 +29,21 @@ public class Grid extends JFrame
         //setting JFrame options
         setTitle("Pokemon Tic Tac Toe");
         setContentPane(panel);
-        setSize(400,400);
+        setSize(400, 400);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        for(int i = 0; i<3; i++)
+        {
+            for(int j = 0; j<3; j++)
+            {
+                if(e.getSource() == buttonArray[i][j])
+                {
+                    System.out.println("Button at " + i + " " + j + " pressed");
+                }
+            }
+        }
     }
 }
---
+++ Icon.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+public class Icon
+{
+        public static final ImageIcon zeroIcon = new ImageIcon("nought.jpg");
+        public static final ImageIcon crossIcon = new ImageIcon("cross.jpg");
+}
